063050a9f576a8776a7295c92eef57d6c5d683f6
Brian Leighton <brian@bigroomstudios.com>
2018-02-25 00:35:48 -0500

    stuff

---
+++ actions/beer.js
@@ -0,0 +1,51 @@
+const WebClient = require('../utils/web-client');
+const BeerTypes = require('../action-types/beer');
+
+const internals = {};
+const actions = exports;
+
+exports.beersLoadBegin = () => {
+
+    return {
+        type: BeerTypes.BEERS_LOAD_BEGIN
+    };
+};
+
+exports.beersLoadSuccess = (beers) => {
+
+    return {
+        type: BeerTypes.BEERS_LOAD_SECCESS,
+        payload: beers
+    };
+};
+
+exports.beersLoadError = (error) => {
+
+    return {
+        type: BeerTypes.BEERS_LOAD_ERROR,
+        payload: { error },
+        error: true
+    };
+};
+
+exports.loadBeer = () => {
+
+    return (dispatch) => {
+
+        dispatch(actions.beersLoadBegin());
+
+        const getBeers = WebClient.get('/beers/on-tap');
+
+        getBeers
+        .then(({ data }) => {
+
+            dispatch(actions.beersLoadSuccess(data));
+        })
+        .catch((error) => {
+
+            dispatch(actions.beersLoadError(error));
+        });
+
+        return getBeers;
+    };
+};
